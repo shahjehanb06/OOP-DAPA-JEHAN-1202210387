68127e7b5469c4261fd83da0c286e19f31792416
Shah Jehan Basrul <107973437+shahjehanb06@users.noreply.github.com>
2022-11-04 16:48:58 +0700

    Add files via upload

---
+++ OOP_DAPA_JEHAN/User.java
@@ -0,0 +1,24 @@
+public class User {
+
+    // TODO Create Attribute of User; Name and Phone Number then Create Setter
+
+    String nama;
+    int Phone_number;
+
+    public void setnama (String nama){
+        this.nama = nama;
+    }
+    public void setphone_number (int phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    // TODO Create Method to Register User and Display User's Name and Phone Number and success message
+
+    
+
+}
+
+
+
+
+
